e0f54ff8511fa4b36fc63f71a48d7064c47ab331
Muhammed CELIK <muhammed.celik@adayazilim.com>
2023-10-16 21:34:25 +0300

    Added new GrandingStudents project

---
+++ GradingStudents/Program.cs
@@ -0,0 +1,43 @@
+﻿ TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int gradesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> grades = new List<int>();
+
+        for (int i = 0; i < gradesCount; i++)
+        {
+            int gradesItem = Convert.ToInt32(Console.ReadLine().Trim());
+            grades.Add(gradesItem);
+        }
+
+        List<int> result = Result.gradingStudents(grades);
+
+        textWriter.WriteLine(String.Join("\n", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+
+class Result
+{
+
+    /*
+     * Complete the 'gradingStudents' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY grades as parameter.
+     */
+
+    public static List<int> gradingStudents(List<int> grades)
+    {
+        for (int i = 0; i < grades.Count; i++)
+        {
+            if (grades[i] < 38)
+                continue;
+            var nextMultipleOf5 = grades[i] + 5 -(grades[i] % 5);
+            var difference = nextMultipleOf5 - grades[i];
+            if (difference < 3)
+                grades[i] = nextMultipleOf5;
+        }
+        return grades;
+    }
+}
